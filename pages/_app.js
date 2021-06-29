8cd3a3c40a9a724f917f5919b4e21d6908a476ff
Puncharas Phosap <lucifer2002x@gmail.com>
2021-06-29 23:24:26 +0700

    Awesomesss

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import '../styles/globals.scss'
 import Head from 'next/head'
+import Script from 'next/script'
 function MyApp({ Component, pageProps }) {
   return <>
   <Head>
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"></script>
+  <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"></Script>
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
   <link href="https://fonts.googleapis.com/css2?family=Concert+One&display=swap" rel="stylesheet"></link>
